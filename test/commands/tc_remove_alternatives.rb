393e4e02fa7dd279c34d6a319d270cf3d92b0a39
Magnar Sveen <magnars@gmail.com>
2012-03-26 16:15:32 +0200

    New form: remove_alternatives

--- test/commands/tc_remove_alternatives.rb
+++ test/commands/tc_remove_alternatives.rb
@@ -1,4 +1,5 @@
 require 'test/unit'
+require 'room'
 require 'commands/remove_alternatives'
 
 class CommandsRemoveAlternativesTestCase < Test::Unit::TestCase
@@ -6,6 +7,13 @@ class CommandsRemoveAlternativesTestCase < Test::Unit::TestCase
   def test_should_remove_alternatives
     command = RemoveAlternatives.parse?("#23")
     assert_equal('if ($this->con(!$this->receiver->has_flag("nr23"))) { $this->receiver->add_flag("nr23"); }', command.code)
+  end
+
+  def test_should_remove_alternatives_to_current_room
+    room = ["#!"].extend(Room)
+    room.number = 17
+    command = RemoveAlternatives.parse?("#!", room)
+    assert_equal('if ($this->con(!$this->receiver->has_flag("nr17"))) { $this->receiver->add_flag("nr17"); }', command.code)
   end
 
 end
