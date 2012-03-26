68bcb6f38faf01c1a9aeff52e300c7bc41096cab
Magnar Sveen <magnars@gmail.com>
2012-03-26 16:03:47 +0200

    New form: plain_text

--- lib/commands/plain_text.rb
+++ lib/commands/plain_text.rb
@@ -1,27 +1,18 @@
 class PlainText
   def initialize(text)
-    @text = add_spaces_every_eighty(text)
+    @text = text
   end
-  
-  def add_spaces_every_eighty(text)
-    chars = text.split(//u)
-    if chars.length > 80 then
-      [chars[0..79], " ", add_spaces_every_eighty(chars[80..-1].join)].join
-    else
-      text
-    end
-  end
-  
+
   def self.parse?(line, room = nil, enterpreter = nil)
     new(line)
   end
-  
+
   def code
     "$this->receiver->write(\"#{escape(@text)}\");"
   end
-  
+
   def escape(text)
     text.gsub('\\', ':BS::BS::BS::BS::BS:').gsub(':BS:', '\\').gsub('"', '\"').gsub("<navn>", '".$this->receiver->get_nickname()."')
   end
-  
+
 end
--- test/commands/tc_plain_text.rb
+++ test/commands/tc_plain_text.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'test/unit'
 require 'commands/plain_text'
 
@@ -15,9 +16,9 @@ class CommandsPlainTextTestCase < Test::Unit::TestCase
     assert_equal('$this->receiver->write("a line of prose");', @text.code)
   end
 
-  def test_should_add_spaces_after_every_80_chars
-    assert_equal('$this->receiver->write("\"..og for å skape en troverdig front for dette mørkeste av ritualer, så skal de  laveste medlemmene av banklosjen arrangere en liksomfest med liksompunsj og      liksomdansing etterpå. For en farse! Hadde det ikke vært for mitt dårlige kne så skulle jeg satt en stopper for det mørke ritualet selv.\" Sjømannen nikker ivrig, veldig enig med seg selv.");', 
-        PlainText.parse?('"..og for å skape en troverdig front for dette mørkeste av ritualer, så skal de laveste medlemmene av banklosjen arrangere en liksomfest med liksompunsj og     liksomdansing etterpå. For en farse! Hadde det ikke vært for mitt dårlige kne såskulle jeg satt en stopper for det mørke ritualet selv." Sjømannen nikker ivrig,veldig enig med seg selv.').code)
+  def test_should_not_add_spaces_after_every_80_chars_anymore
+    assert_equal('$this->receiver->write("\"..og for å skape en troverdig front for dette mørkeste av ritualer, så skal de laveste medlemmene av banklosjen arrangere en liksomfest med liksompunsj og     liksomdansing etterpå. For en farse! Hadde det ikke vært for mitt dårlige kne så skulle jeg satt en stopper for det mørke ritualet selv.\" Sjømannen nikker ivrig, veldig enig med seg selv.");',
+                 PlainText.parse?('"..og for å skape en troverdig front for dette mørkeste av ritualer, så skal de laveste medlemmene av banklosjen arrangere en liksomfest med liksompunsj og     liksomdansing etterpå. For en farse! Hadde det ikke vært for mitt dårlige kne så skulle jeg satt en stopper for det mørke ritualet selv." Sjømannen nikker ivrig, veldig enig med seg selv.').code)
   end
 
   def test_should_escape_quotation_marks
@@ -28,9 +29,9 @@ class CommandsPlainTextTestCase < Test::Unit::TestCase
     # denne sinnsyke mengden backslasher er det som skal til for å få et dobbelt sett helt ut til eksporten..
     assert_equal('$this->receiver->write("Backslash: \\\\\\\\\");', PlainText.new("Backslash: \\").code)
   end
-  
+
   def test_should_insert_nickname
-    assert_equal('$this->receiver->write("You are known as ".$this->receiver->get_nickname()." the great.");', 
+    assert_equal('$this->receiver->write("You are known as ".$this->receiver->get_nickname()." the great.");',
                  PlainText.new("You are known as <navn> the great.").code)
   end
 
