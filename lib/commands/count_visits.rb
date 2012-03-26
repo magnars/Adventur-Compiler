bdff4036bec6648502229728a69b745a4c28fa26
Magnar Sveen <magnars@gmail.com>
2012-03-26 15:50:23 +0200

    New form: count_visits

--- lib/commands/count_visits.rb
+++ lib/commands/count_visits.rb
@@ -1,32 +1,26 @@
+# -*- coding: utf-8 -*-
 class CountVisits
-  
+
   def self.parse?(line, room, enterpreter = nil)
-    if line === "[Y]"
-      new room.number
-    elsif line =~ /^\[Y\](\d+)$/
+    if line =~ /^:besøk \+\+ \(max (\d+)\)/
       new room.number, $1
     else
       false
     end
   end
-  
-  def initialize(room_number, limit = nil)
+
+  def initialize(room_number, limit)
     @room_number, @limit = room_number, limit
   end
-  
+
   def code
     wrap_in_if("$this->receiver->add_to_detail(\"\\$_VISITS_TO_#{@room_number}\", 1);")
   end
-  
+
   private
-  
+
   def wrap_in_if(statement)
-    if @limit
-      "if ($this->con($this->receiver->get_detail(\"\\$_VISITS_TO_#{@room_number}\") < #{@limit})) { #{statement} }"
-    else
-      statement
-    end
+    "if ($this->con($this->receiver->get_detail(\"\\$_VISITS_TO_#{@room_number}\") < #{@limit})) { #{statement} }"
   end
-  
-  
+
 end
--- test/commands/tc_count_visits.rb
+++ test/commands/tc_count_visits.rb
@@ -1,18 +1,12 @@
+# -*- coding: utf-8 -*-
 require 'test/unit'
 require 'commands/count_visits'
 require 'room'
 
 class CommandsCountVisitsTestCase < Test::Unit::TestCase
 
-  def test_should_count_unlimited_visits
-    room = ['[Y]'].extend(Room)
-    room.number = 127
-    command = CountVisits.parse?(room.current, room)
-    assert_equal('$this->receiver->add_to_detail("\$_VISITS_TO_127", 1);', command.code)
-  end
-
   def test_should_count_limited_visits
-    room = ['[Y]4'].extend(Room)
+    room = [':besøk ++ (max 4)'].extend(Room)
     room.number = 7
     command = CountVisits.parse?(room.current, room)
     assert_equal('if ($this->con($this->receiver->get_detail("\$_VISITS_TO_7") < 4)) { $this->receiver->add_to_detail("\$_VISITS_TO_7", 1); }', command.code)
