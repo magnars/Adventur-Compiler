c2d517cfa6a42d43d90549f51646a9bb42648fdf
Magnar Sveen <magnars@gmail.com>
2012-03-26 15:40:38 +0200

    New form: add_timejump

--- lib/commands/add_timejump.rb
+++ lib/commands/add_timejump.rb
@@ -1,15 +1,18 @@
 class AddTimejump
-  
+  def initialize(num)
+    @num = num
+  end
+
   def self.parse?(line, room = nil, enterpreter = nil)
-    if line === "$$$"
-      new
+    if line.sub!(/^:tidshopp /, "")
+      new(line.to_i)
     else
       false
     end
   end
-  
+
   def code
-    '$this->receiver->add_timejump();'
+    "$this->receiver->add_timejumps(#{@num});"
   end
-  
+
 end
--- test/commands/tc_add_timejump.rb
+++ test/commands/tc_add_timejump.rb
@@ -4,11 +4,11 @@ require 'commands/add_timejump'
 class CommandsAddTimejumpTestCase < Test::Unit::TestCase
 
   def setup
-    @command = AddTimejump.parse?("$$$")
+    @command = AddTimejump.parse?(":tidshopp +3")
   end
 
   def test_should_build_code
-    assert_equal('$this->receiver->add_timejump();', @command.code)
+    assert_equal('$this->receiver->add_timejumps(3);', @command.code)
   end
 
 end
