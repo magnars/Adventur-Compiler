5d34fc3d78d9f5b1343b123d12a3129e118f9289
Magnar Sveen <magnars@gmail.com>
2012-03-26 15:54:27 +0200

    New form: end_subpage

--- lib/commands/end_subpage.rb
+++ lib/commands/end_subpage.rb
@@ -1,14 +1,14 @@
 class DramaticPause
   def self.parse?(line, room = nil, enterpreter = nil)
-    if line === "!!!"
+    if line[0..2] == "---"
       new
     else
       false
     end
   end
-  
+
   def code
     "$this->receiver->end_subpage();"
   end
-  
+
 end
--- test/commands/tc_end_subpage.rb
+++ test/commands/tc_end_subpage.rb
@@ -4,11 +4,18 @@ require 'commands/end_subpage'
 class CommandsDramaticPauseTestCase < Test::Unit::TestCase
 
   def setup
-    @command = DramaticPause.parse?("!!!")
+    @command1 = DramaticPause.parse?("--")
+    @command2 = DramaticPause.parse?("---")
+    @command3 = DramaticPause.parse?("-------")
+  end
+
+  def test_should_not_end_subpage_with_few
+    assert(!@command1)
   end
 
   def test_should_end_the_subpage
-    assert_equal('$this->receiver->end_subpage();', @command.code)
+    assert_equal('$this->receiver->end_subpage();', @command2.code)
+    assert_equal('$this->receiver->end_subpage();', @command3.code)
   end
 
 end
