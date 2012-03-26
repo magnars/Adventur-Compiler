9da93050afe83bf4cc8fd1d6a9084fbfd4b3daf6
Magnar Sveen <magnars@gmail.com>
2012-03-26 15:59:21 +0200

    New form: increase_bodycount

--- test/commands/tc_increase_bodycount.rb
+++ test/commands/tc_increase_bodycount.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'test/unit'
 require 'commands/increase_bodycount'
 require 'room'
@@ -5,16 +6,16 @@ require 'room'
 class CommandsIncreaseBodycountTestCase < Test::Unit::TestCase
 
   def setup
-    room = ['%%%', '3'].extend(Room)
+    room = [':drap +3'].extend(Room)
     @command = IncreaseBodycount.parse?(room.current, room)
   end
 
   def test_should_generate_code
     assert_equal('$this->receiver->add_to_detail("\$_BODYCOUNT", 3);', @command.code)
   end
-  
+
   def test_should_complain_about_illegal_values
-    room = ['%%%', 'Du får en bodycount men det bør helst være et tall her også.'].extend(Room)
+    room = [':drap Du får en bodycount men det bør helst være et tall her også.'].extend(Room)
     assert_raise(RuntimeError) { IncreaseBodycount.parse?(room.current, room) }
   end
 
