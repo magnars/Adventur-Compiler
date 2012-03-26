df6f28d2f77667817882181e167fc2901d773394
Magnar Sveen <magnars@gmail.com>
2012-03-26 16:16:19 +0200

    New form: remove_flag

--- test/commands/tc_remove_flag.rb
+++ test/commands/tc_remove_flag.rb
@@ -4,12 +4,12 @@ require 'commands/remove_flag'
 class CommandsRemoveFlagTestCase < Test::Unit::TestCase
 
   def test_should_remove_normal_flags
-    command = RemoveFlag.parse?(")(FLAG")
+    command = RemoveFlag.parse?("-- FLAG")
     assert_equal('if ($this->con($this->receiver->has_flag("FLAG"))) { $this->receiver->remove_flag("FLAG"); }', command.code)
   end
 
   def test_should_remove_value_flags
-    command = RemoveFlag.parse?(")($VAL")
+    command = RemoveFlag.parse?("-- $VAL")
     assert_equal('if ($this->con($this->receiver->has_flag("\$VAL"))) { $this->receiver->remove_flag("\$VAL"); }', command.code)
   end
 
