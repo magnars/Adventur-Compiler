95a836165097bcdd1560c4c9487a2321f041fdd1
Magnar Sveen <magnars@gmail.com>
2012-03-28 06:39:13 +0200

    Ported condition parser from clojure.

---
+++ lib/cond_tree.rb
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+require "conditional"
+
+class CondTree
+  def self.parse(s)
+    strip_out_containers(req_parse(replace_with_oldies(s), [:container]))
+  end
+
+  def self.cons(el, list)
+    list.unshift(el)
+  end
+
+  def self.size(s)
+    s.split(//u).size
+  end
+
+  def self.subs(s, from, to = nil)
+    cs = s.split(//u)
+    return cs[from..-1].join if to.nil?
+    cs[from...to].join
+  end
+
+  def self.req_parse(s, top)
+    return top if (s.empty?)
+    return parse_single(s, top) if next_is_single_node(s)
+    return [:and, top, parse(subs(s, 4))] if s.start_with? " og "
+    return [:or, top, parse(subs(s, 7))] if s.start_with? " eller "
+    return [:not_this_without_that, pop_off_not(top), parse(subs(s, 11))] if s.start_with? " uten også "
+    return [:this_but_not_that, top, parse(subs(s, 11))] if s.start_with? ", men ikke "
+    return [:and, top, parse(subs(s, 6))] if s.start_with? ", men "
+    raise "Unable to parse: #{s}"
+  end
+
+  def self.parse_single(s, top)
+    return parse_flag(s, top) if s =~ flag_regexp
+    return parse_all(s, top) if s =~ all_regexp
+    return parse_some(s, top) if s =~ some_regexp
+    return parse_parens(s, top) if s.start_with? "("
+    return top.push(parse_single(subs(s, 5), [:not])) if s.start_with? "ikke "
+    raise "Unable to parse single: #{s}"
+  end
+
+  def self.next_is_single_node(s)
+    (s =~ flag_regexp) or
+      (s =~ all_regexp) or
+      (s =~ some_regexp) or
+      (s.start_with? "(") or
+      (s.start_with? "ikke ")
+  end
+
+  def self.pop_off_not(top)
+    raise "Expected :not, but was #{top[0]}" unless top[0] == :not
+      top[1]
+  end
+
+  def self.parse_parens(s, top)
+    req_parse(subs(s, 1 + find_matching_paren_index(s, 1)),
+              top.push(parse(subs(s, 1, find_matching_paren_index(s, 1)))))
+  end
+
+  def self.parse_flag(s, top)
+    match = get_flag(s)
+    req_parse(subs(s, size(match)),
+              top.push(match))
+  end
+
+  def self.parse_all(s, top)
+    match = get_all(s)
+    req_parse(subs(s, size(match)),
+              top.push(cons(:all, subs(match, 5).split(/(?:, | og )/))))
+  end
+
+  def self.parse_some(s, top)
+    match = get_some(s)
+    req_parse(subs(s, size(match)),
+              top.push(cons(:some, subs(match, 6).split(/(?:, | eller )/))))
+  end
+
+  def self.find_matching_paren_index(s, i)
+    old_i = i
+    cs = s.split(//u)
+    num_start = 1
+    i = i - 1
+    until num_start == 0 do
+      i = i + 1
+      num_start = num_start + 1 if cs[i] == "("
+      num_start = num_start - 1 if cs[i] == ")"
+      raise "found no matching paren from #{old_i} in: #{s}" if cs[i].nil?
+    end
+    i
+  end
+
+  def self.strip_out_containers(n)
+    return n if n.class == String
+    return n if n.class == Symbol
+    return strip_out_containers(n[1]) if n[0] == :container
+    n.map { |e| strip_out_containers(e) }
+  end
+
+  def self.replace_with_oldies(s)
+    s = replace_with_old_date(s)
+    s = replace_with_old_alt_req(s)
+    replace_neither(s)
+  end
+
+  def self.replace_neither(s)
+    s = s.gsub("(verken ", "ikke (enten ")
+    s.gsub("verken ", "ikke enten ")
+  end
+
+  def self.replace_with_old_alt_req(s)
+    s.gsub(/(\d)\. alternativ/) { |i| "*#{i.to_i - 1}*" }
+  end
+
+  def self.replace_with_old_date(s)
+    s.gsub(/den (\d\d)\/(\d\d)/) { "DATO#{$1}#{$2}" }
+  end
+
+  def self.get_some(s)
+    get_leading(":some", some_regexp, s)
+  end
+
+  def self.some_regexp()
+    /^enten (?:\$?[A-ZÆØÅ0-9.*_-]+(?:, )?)+ eller \$?[A-ZÆØÅ0-9.*_-]+/
+  end
+
+  def self.get_all(s)
+    get_leading(":all", all_regexp, s)
+  end
+
+  def self.all_regexp()
+    /^både (?:\$?[A-ZÆØÅ0-9.*_-]+(?:, )?)+ og \$?[A-ZÆØÅ0-9.*_-]+/
+  end
+
+  def self.get_flag(s)
+    get_leading("flag", flag_regexp, s)
+  end
+
+  def self.flag_regexp()
+    /^\$?[A-ZÆØÅ0-9.*_-]+/
+  end
+
+  def self.get_leading(name, regexp, s)
+    f = s.match(regexp)
+    raise "#{s} is not recognizeable as a #{name}" if f.nil?
+    #puts "got leading #{name}: #{f.to_s}"
+    f.to_s
+  end
+
+end
---
+++ test/tc_cond_tree.rb
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+require 'test/unit'
+require 'cond_tree'
+
+class CondTreeTestCase < Test::Unit::TestCase
+
+  def test_parse_flags
+    assert_equal("BØLLE", CondTree.parse("BØLLE"))
+  end
+
+  def test_parse_nots
+    assert_equal([:not, "BØLLE"], CondTree.parse("ikke BØLLE"))
+  end
+
+  def test_replace_with_oldies
+    assert_equal("DATO2412", CondTree.parse("den 24/12"))
+  end
+
+  def test_parse_all_two
+    assert_equal([:all, "TØY", "DATO0412"],
+                 CondTree.parse("både TØY og den 04/12"))
+  end
+
+  def test_parse_all_more
+    assert_equal([:all, "TØY1", "TØY2", "*1*"],
+                 CondTree.parse("både TØY1, TØY2 og 2. alternativ"))
+  end
+
+  def test_parse_some
+    assert_equal([:some, "TØY1", "TØY2", "*1*"],
+                 CondTree.parse("enten TØY1, TØY2 eller 2. alternativ"))
+  end
+
+  def test_strip_parens
+    assert_equal("BØLLE", CondTree.parse("(BØLLE)"))
+  end
+
+  def test_parse_but_without_parens
+    assert_equal([:this_but_not_that, "BØY1", "BØY2"],
+                 CondTree.parse("BØY1, men ikke BØY2"))
+  end
+
+  def test_parse_but_without_parens_complex
+    assert_equal([:this_but_not_that, [:all, "PÅ1", "PÅ2", "PÅ3"], [:all, "PÅ4", "PÅ5"]],
+                 CondTree.parse("både PÅ1, PÅ2 og PÅ3, men ikke både PÅ4 og PÅ5"))
+  end
+
+  def test_parse_not_this_without_that
+    assert_equal([:not_this_without_that, "BØY1", "BØY2"],
+                 CondTree.parse("ikke BØY1 uten også BØY2"))
+  end
+
+  def test_parse_not_this_without_that_complex
+    assert_equal([:not_this_without_that, [:some, "PÅ1", "PÅ2"], [:all, "PÅ3", "PÅ4"]],
+                 CondTree.parse("verken PÅ1 eller PÅ2 uten også både PÅ3 og PÅ4"))
+  end
+
+  def test_parse_not_this_without_that_complex_2
+    assert_equal([:not_this_without_that, [:some, "PÅ1", "PÅ2"], [:and, "PÅ3", "PÅ4"]],
+                 CondTree.parse("verken PÅ1 eller PÅ2 uten også PÅ3 og PÅ4"))
+  end
+
+  def test_parse_and
+    assert_equal([:and, "TØY1", "TØY2"], CondTree.parse("TØY1 og TØY2"))
+  end
+
+  def test_parse_parens
+    assert_equal([:and, [:not, "PÅ1"], [:all, "PÅ2", "PÅ3"]],
+                 CondTree.parse("(ikke PÅ1) og (både PÅ2 og PÅ3)"))
+  end
+
+  def test_parse_and_without_parens
+    assert_equal([:and, [:not, "PÅ1"], [:all, "PÅ2", "PÅ3"]],
+                 CondTree.parse("ikke PÅ1 og både PÅ2 og PÅ3"))
+  end
+
+  def test_parse_equals_and
+    assert_equal([:and, [:not, "PÅ1"], [:all, "PÅ2", "PÅ3"]],
+                 CondTree.parse("ikke PÅ1, men både PÅ2 og PÅ3"))
+  end
+
+  def test_parse_basic_or
+    assert_equal([:or, "TØY1", "TØY2"],
+                 CondTree.parse("TØY1 eller TØY2"))
+  end
+
+  def test_parse_or_with_parens
+    assert_equal([:or, [:not, "PÅ1"], [:all, "PÅ2", "PÅ3"]],
+                 CondTree.parse("(ikke PÅ1) eller (både PÅ2 og PÅ3)"))
+  end
+
+  def test_parse_or_without_parens
+    assert_equal([:or, [:not, "PÅ1"], [:all, "PÅ2", "PÅ3"]],
+                 CondTree.parse("ikke PÅ1 eller både PÅ2 og PÅ3"))
+  end
+
+  def test_parens_needed
+    assert_equal([:and, [:or, "Æ1", "Æ2"], [:or, "Æ3", "Æ4"]],
+                 CondTree.parse("(Æ1 eller Æ2) og (Æ3 eller Æ4)"))
+  end
+
+  def test_parens_skipped
+    assert_equal([:or, "Æ1", [:and, "Æ2", [:or, "Æ3", "Æ4"]]],
+                 CondTree.parse("Æ1 eller Æ2 og Æ3 eller Æ4"))
+  end
+
+  def test_not_all
+    assert_equal([:not, [:all, "FØY1", "FØY2"]],
+                 CondTree.parse("ikke både FØY1 og FØY2"))
+  end
+
+  def test_not_some
+    assert_equal([:not, [:some, "FØY1", "FØY2"]],
+                 CondTree.parse("ikke enten FØY1 eller FØY2"))
+  end
+
+  def test_neither
+    assert_equal([:not, [:some, "FØY1", "FØY2"]],
+                 CondTree.parse("verken FØY1 eller FØY2"))
+  end
+
+  def test_neither_with_parens
+    assert_equal([:not, [:some, "FØY1", "FØY2"]],
+                 CondTree.parse("(verken FØY1 eller FØY2)"))
+  end
+
+  def test_not_neither_with_parens
+    assert_equal([:not, [:not, [:some, "FØY1", "FØY2"]]],
+                 CondTree.parse("ikke (verken FØY1 eller FØY2)"))
+  end
+
+end
